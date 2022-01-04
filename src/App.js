e1158ae58edb363112cd58b4d64eed38ae30d0e5
alina1030 <85539852+alina1030@users.noreply.github.com>
2022-01-04 09:44:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Display from './components/Display';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Display></Display>
+    </div>
+  );
+}
+
+export default App;
